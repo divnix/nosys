05316dea113bd58802aeffa5bd5dbf29b1232ffb
David Arnold <david.arnold@iohk.io>
2022-11-07 20:47:53 -0500

    fix: the double system
    closes: #2

--- eachSys.nix
+++ eachSys.nix
@@ -6,6 +6,81 @@
 # except for the `hydraJobs` attribute, where it maps the inner attributes,
 # from hydraJobs.<attr>=value to hydraJobs.<attr>.<system>=value.
 #
+let
+  # nixpkgs.lib.systems.doubles.all
+  allKnownSystems = [
+    "i686-cygwin"
+    "x86_64-cygwin"
+    "x86_64-darwin"
+    "i686-darwin"
+    "aarch64-darwin"
+    "armv7a-darwin"
+    "i686-freebsd"
+    "x86_64-freebsd"
+    "aarch64-genode"
+    "i686-genode"
+    "x86_64-genode"
+    "x86_64-solaris"
+    "js-ghcjs"
+    "aarch64-linux"
+    "armv5tel-linux"
+    "armv6l-linux"
+    "armv7a-linux"
+    "armv7l-linux"
+    "i686-linux"
+    "m68k-linux"
+    "microblaze-linux"
+    "microblazeel-linux"
+    "mipsel-linux"
+    "mips64el-linux"
+    "powerpc64-linux"
+    "powerpc64le-linux"
+    "riscv32-linux"
+    "riscv64-linux"
+    "s390-linux"
+    "s390x-linux"
+    "x86_64-linux"
+    "mmix-mmixware"
+    "aarch64-netbsd"
+    "armv6l-netbsd"
+    "armv7a-netbsd"
+    "armv7l-netbsd"
+    "i686-netbsd"
+    "m68k-netbsd"
+    "mipsel-netbsd"
+    "powerpc-netbsd"
+    "riscv32-netbsd"
+    "riscv64-netbsd"
+    "x86_64-netbsd"
+    "aarch64_be-none"
+    "aarch64-none"
+    "arm-none"
+    "armv6l-none"
+    "avr-none"
+    "i686-none"
+    "microblaze-none"
+    "microblazeel-none"
+    "msp430-none"
+    "or1k-none"
+    "m68k-none"
+    "powerpc-none"
+    "powerpcle-none"
+    "riscv32-none"
+    "riscv64-none"
+    "rx-none"
+    "s390-none"
+    "s390x-none"
+    "vc4-none"
+    "x86_64-none"
+    "i686-openbsd"
+    "x86_64-openbsd"
+    "x86_64-redox"
+    "wasm64-wasi"
+    "wasm32-wasi"
+    "x86_64-windows"
+    "i686-windows"
+  ];
+in
 systems: f: let
   # Taken from <nixpkgs/lib/attrsets.nix>
   isDerivation = x: builtins.isAttrs x && x ? type && x.type == "derivation";
@@ -43,7 +118,7 @@ systems: f: let
         then ret.${key}
         else if key == "hydraJobs"
         then (pushDownSystem system (attrs.hydraJobs or {}) ret.hydraJobs)
-        else {${system} = ret.${key};};
+        else {${system} = (builtins.removeAttrs ret.${key} allKnownSystems);};
     in
       attrs
       // {
