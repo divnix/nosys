0a2b8ef61777bce1d2c5d13efee088a0e9be86e3
Timothy DeHerrera <tim@nrdxp.dev>
2024-02-24 02:33:59 -0700

    feat: specially handle `pkgs` input
    If nixpkgs is declared in a flake as the `pkgs` input, then the
    package collection will automatically be imported under the given
    input for quick access, e.g. `pkgs.zlib` instead of
    `pkgs.legacyPackages.zlib`.
    
    In addition, the `lib` attribute will be brought into the top-level of
    the functor arguments for quick reference, e.g.
    `nosys inputs ({lib, pkgs, ...}: {})`

--- flake.nix
+++ flake.nix
@@ -6,20 +6,49 @@
   outputs = {self}: {
     __functor = self: self.lib.noSys;
 
-    lib.noSys = inputs @ {systems ? import ./systems.nix, ...}: f: let
+    lib.noSys = inputs @ {
+      systems ? import ./systems.nix,
+      config ? {},
+      ...
+    }: f: let
       systems' =
         if builtins.isPath systems || systems ? outPath
         then import systems
         else systems;
     in
-      self.lib.eachSys systems' (sys: let
+      self.lib.eachSys systems' (system: let
         f' = inputs: let
           # mapAttrs to deSys up to the same depths as in `self`
-          inputs' = builtins.mapAttrs (_: self.lib.deSys sys) (builtins.removeAttrs inputs ["self"]);
-          self' = self.lib.deSys sys (builtins.removeAttrs inputs.self ["inputs"]);
+          inputs' = builtins.mapAttrs (k: v: let
+            input = self.lib.deSys system v;
+          in
+            if
+              k
+              == "pkgs"
+              && v ? legacyPackages
+              && v.inputs == {}
+              && builtins.pathExists "${v}/default.nix"
+            then let
+              pkgs =
+                if inputs ? config && inputs.config != {}
+                then
+                  import v {
+                    inherit system config;
+                  }
+                else input.legacyPackages;
+            in
+              pkgs // input
+            else input) (builtins.removeAttrs inputs ["self"]);
+          self' = self.lib.deSys system (builtins.removeAttrs inputs.self ["inputs"]);
         in
           # must be recombined after `deSys` to avoid infinite recursion
-          f (inputs' // {self = self';});
+          f ((
+              if inputs ? pkgs && inputs.pkgs ? lib
+              then {inherit (inputs.pkgs) lib;}
+              else {}
+            )
+            // inputs'
+            // {self = self';});
       in
         f' inputs);
 
